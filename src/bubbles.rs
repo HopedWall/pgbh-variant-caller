01961ac744a8f31cf4467d59cbf40c9e6095ce9b
HopedWall <francesco.porto97@gmail.com>
2021-12-09 18:36:02 +0100

    Completed bubble detection

--- src/bubbles.rs
+++ src/bubbles.rs
@@ -4,11 +4,13 @@ use handlegraph::hashgraph::HashGraph;
 use itertools::Itertools;
 use std::collections::VecDeque;
 
+#[derive(Debug)]
 pub enum BubbleType {
     Simple,
     Complex,
 }
 
+#[derive(Debug)]
 pub struct Bubble {
     start: u64,
     end: u64,
@@ -38,13 +40,12 @@ fn advance_bfs(graph: &HashGraph, curr_front: Vec<Handle>) -> Vec<Handle> {
 }
 
 pub fn find_bubbles(graph: &HashGraph) -> Vec<Bubble> {
-    let bubbles: Vec<Bubble> = vec![];
+    let mut bubbles: Vec<Bubble> = vec![];
 
     // Run a BFS visit of the graph, using a Queue
     let handles: Vec<Handle> = graph.handles_iter().sorted().collect();
-    //let mut curr_width_queue: VecDeque<Handle> = VecDeque::new();
-    //let mut next_width_queue: VecDeque<Handle> = VecDeque::new();
     let mut bfs_tree_widths: Vec<usize> = vec![];
+    let mut fronts: Vec<Vec<Handle>> = vec![];
 
     if !handles.is_empty() {
         // We know that the graph starts with a single node
@@ -52,17 +53,53 @@ pub fn find_bubbles(graph: &HashGraph) -> Vec<Bubble> {
 
         // Initialize the BFS
         let first_handle = handles.first().unwrap().clone();
-        let mut new_front = advance_bfs(graph, vec![first_handle]);
+        let first_front = vec![first_handle];
+        fronts.push(first_front.clone());
+
+        let mut new_front = advance_bfs(graph, first_front.clone());
         bfs_tree_widths.push(new_front.len());
+        fronts.push(new_front.clone());
 
         while !new_front.is_empty() {
             new_front = advance_bfs(graph, new_front);
             if !new_front.is_empty() {
+                fronts.push(new_front.clone());
                 bfs_tree_widths.push(new_front.len());
             }
         }
 
+        println!("Fronts are: {:#?}", fronts);
         println!("Bfs tree widths are: {:#?}", bfs_tree_widths);
+
+        let mut bubble_open: VecDeque<Handle> = VecDeque::new();
+        let mut bubble_close: VecDeque<Handle> = VecDeque::new();
+        let mut curr_handle_id: u64 = 2;
+        for level in 0..bfs_tree_widths.len() - 1 {
+            let width_i = *bfs_tree_widths.get(level).unwrap();
+            let width_j = *bfs_tree_widths.get(level+1).unwrap();
+
+            if width_j > width_i {
+                bubble_open.push_front(Handle::from_integer(curr_handle_id));
+            } else if width_j < width_i {
+                bubble_close.push_front(Handle::from_integer(curr_handle_id + 2*width_i as u64));
+            }
+
+            curr_handle_id += 2*width_i as u64;
+        }
+
+        println!("Bubble opens are: {:#?}", bubble_open);
+        println!("Bubble closes are: {:#?}", bubble_close);
+
+        //assert_eq!(bubble_open.len(), bubble_close.len());
+        for (open, close) in bubble_open.iter().zip(bubble_close.iter()) {
+            bubbles.push(Bubble {
+                start: open.as_integer(),
+                end: close.as_integer(),
+                bubble_type: BubbleType::Simple
+            });
+        }
+
+        println!("Bubbles: {:#?}", bubbles);
 
         /*
         loop {
@@ -99,10 +136,7 @@ mod test {
     /// FWD  1: A         4: GCA
     ///          \ 3: GA |
     ///
-    ///          | 2: AG \
-    /// REV  1: T         4: TGC
-    ///          \ 3: TC |
-    fn create_simple_graph() -> HashGraph {
+    fn create_simple_graph_bubble() -> HashGraph {
         let mut graph: HashGraph = HashGraph::new();
 
         let h1 = graph.create_handle("A".as_bytes(), 1);
@@ -128,9 +162,41 @@ mod test {
         graph
     }
 
+    /// This function creates a simple graph, used for debugging
+    fn create_simple_graph_superbubble() -> HashGraph {
+        let mut graph: HashGraph = HashGraph::new();
+
+        let h1 = graph.create_handle("AAA".as_bytes(), 1);
+        let h2 = graph.create_handle("CT".as_bytes(), 2);
+        let h3 = graph.create_handle("GA".as_bytes(), 3);
+        let h4 = graph.create_handle("CC".as_bytes(), 4);
+        let h5 = graph.create_handle("TTT".as_bytes(), 5);
+        let h6 = graph.create_handle("A".as_bytes(), 6);
+        let h7 = graph.create_handle("GCA".as_bytes(), 7);
+
+        graph.create_edge(&Edge(h1, h2));
+        graph.create_edge(&Edge(h1, h3));
+
+        graph.create_edge(&Edge(h2, h4));
+        graph.create_edge(&Edge(h2, h5));
+        graph.create_edge(&Edge(h4, h6));
+        graph.create_edge(&Edge(h5, h6));
+        graph.create_edge(&Edge(h6, h7));
+
+        graph.create_edge(&Edge(h3, h7));
+
+        graph
+    }
+
     #[test]
-    fn test() {
-        let graph = create_simple_graph();
+    fn test_bubble() {
+        let graph = create_simple_graph_bubble();
+        find_bubbles(&graph);
+    }
+
+    #[test]
+    fn test_superbubble() {
+        let graph = create_simple_graph_superbubble();
         find_bubbles(&graph);
     }
 }
